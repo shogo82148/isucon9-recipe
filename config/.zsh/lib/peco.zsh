7db1552bf232ecad42789ee32ee18928f9eba7ac
Manabu Seki <github@zitaku.net>
2019-09-07 18:41:17 +0900

    Add zsh peco setting

---
+++ config/.zsh/lib/peco.zsh
@@ -0,0 +1,48 @@
+# history
+function peco-select-history() {
+local tac
+if which tac > /dev/null; then
+  tac="tac"
+else
+  tac="tail -r"
+fi
+BUFFER=$(\history -n 1 | \
+  eval $tac | \
+  peco --query "$LBUFFER")
+CURSOR=$#BUFFER
+zle clear-screen
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history
+
+# ghq
+function peco-src () {
+  local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N peco-src
+bindkey '^]' peco-src
+
+#ssh
+function peco-ssh () {
+  local selected_host=$(awk '
+  tolower($1)=="host" {
+    for (i=2; i<=NF; i++) {
+      if ($i !~ "[*?]") {
+        print $i
+      }
+    }
+  }
+  ' ~/.ssh/conf.d/hosts/* | sort | peco --query "$LBUFFER")
+  if [ -n "$selected_host" ]; then
+    BUFFER="ssh ${selected_host}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N peco-ssh
+bindkey '^\' peco-ssh
